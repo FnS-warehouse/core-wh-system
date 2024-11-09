260ff19b7fb9c37d43e9c87d4959a59328a01531
fadellh <fadel.lukman.dev@gmail.com>
2024-11-09 23:02:36 +0700

    sas

---
+++ src/main/java/com/fns/wh_app/WhAppApplication.java
@@ -0,0 +1,13 @@
+package com.fns.wh_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhAppApplication.class, args);
+	}
+
+}
