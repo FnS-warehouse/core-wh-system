2a99aec577cda577bab594c905cfa855fd49c212
fadellh <fadel.lukman.dev@gmail.com>
2024-11-20 22:13:54 +0700

    update topics

--- infrastructure/kubernetes/create-topics.sh
+++ infrastructure/kubernetes/create-topics.sh
@@ -2,5 +2,5 @@
 kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-request --delete --if-exists
 kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-response --delete --if-exists
 
-kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-request --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-response --create --partitions 3 --replication-factor 1 --if-not-exists
