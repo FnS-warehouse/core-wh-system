064db64fb7c9be4d354590c00c2647e60596ee06
fadellh <fadel.lukman.dev@gmail.com>
2024-11-20 22:11:19 +0700

    update topics

--- infrastructure/kubernetes/create-topics.sh
+++ infrastructure/kubernetes/create-topics.sh
@@ -1,12 +1,6 @@
 #local-confluent-kafka-cp-zookeeper-headless
 kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-request --delete --if-exists
 kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-response --delete --if-exists
-kafka-topics --zookeeper $1:2181 --topic restaurant-approval-request --delete --if-exists
-kafka-topics --zookeeper $1:2181 --topic restaurant-approval-response --delete --if-exists
-kafka-topics --zookeeper $1:2181 --topic customer --delete --if-existskube
 
 kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-request --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-response --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic restaurant-approval-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic restaurant-approval-response --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic customer --create --partitions 3 --replication-factor 3 --if-not-exists
